a27171d4f7a4f68bda27c791e33bb998a2e4f125
Shou-Fan Lin <cs423052001@gmail.com>
2026-05-26 09:00:52 +0000

    fix: restore prettier formatting

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import Fastify, { FastifyServerOptions } from 'fastify';
 
 export function buildApp(options: FastifyServerOptions = {}) {
-  const app =      Fastify({
+  const app = Fastify({
     logger: options.logger ?? true,
     ...options
   });
